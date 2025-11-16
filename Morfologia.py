8b3b1494819b3b04bd87c60f05349748f2a1b31b
Lu1sGH <zarate.lozano.luis.axel@gmail.com>
2025-11-15 20:30:03 -0600

    Morfologia

--- Morfologia.py
+++ Morfologia.py
@@ -106,11 +106,21 @@ class Mofologia:
         if len(imagen.shape) == 3:
             msg.alerta_message("El método no admite imágenes a color. La imagen se convertirá a grises para su uso.")
             imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+        
+        _, imagen = cv2.threshold(imagen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        imagen = imagen.astype(np.uint8)
 
-        #Esqueletizacion usando el método de Zhang-Suen
-        sk = cv2.ximgproc.thinning(imagen, thinningType=cv2.ximgproc.THINNING_GUOHALL)
+        skel = np.zeros(imagen.shape, dtype=np.uint8)
+        while True:
+            er = cv2.erode(imagen, self.kernel)
+            temp = cv2.dilate(er, self.kernel)
+            temp = cv2.subtract(imagen, temp)
+            skel = cv2.bitwise_or(skel, temp)
+            imagen = er.copy()
+            if cv2.countNonZero(imagen) == 0:
+                break
 
-        return sk
+        return skel
     
     def tophat(self, imagen): #Calcular la operación tophat de una imagen con openCV
         if len(imagen.shape) == 3:
@@ -137,57 +147,3 @@ class Mofologia:
         erosionada = self.erosionCV(imagen)
         gradiente = cv2.subtract(dilatada, erosionada)
         return gradiente
-"""
-    def esqueleto(self, img):
-        if len(img.shape) == 3:
-            msg.alerta_message("El método no admite imágenes a color. La imagen se convertirá a grises para su uso.")
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        tam = np.size(img)
-        skel = np.zeros(img.shape,np.uint8)
-        
-        _, img = cv2.threshold(img,127,255,0)
-        element = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
-        done = False
-        
-        while not done:
-            erosionada = cv2.erode(img,element)
-            temp = cv2.dilate(erosionada,element)
-            temp = cv2.subtract(img,temp)
-            skel = cv2.bitwise_or(skel,temp)
-            img = erosionada.copy()
-        
-            zeros = tam - cv2.countNonZero(img)
-            if zeros==tam:
-                done = True
-
-        return skel
-
-    def adelgazamiento(self, imagen):
-        if len(imagen.shape) == 3:
-            msg.alerta_message("El método no admite imágenes a color. La imagen se convertirá a grises para su uso.")
-            imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-        
-        kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-        thin = np.zeros(imagen.shape,dtype='uint8')
-        
-
-        img1 = imagen.copy()
-        while (cv2.countNonZero(img1)!=0):
-            er = cv2.erode(img1,kernel)
-            aper = cv2.morphologyEx(er,cv2.MORPH_OPEN,kernel)
-            sub = er - aper
-            thin = cv2.bitwise_or(sub,thin)
-            img1 = er.copy()
-
-        return thin
-    
-    def thinUseless(self, imagen):
-        if len(imagen.shape) == 3:
-            msg.alerta_message("El método no admite imágenes a color. La imagen se convertirá a grises para su uso.")
-            imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-
-        thin = cv2.bitwise_and(imagen, cv2.bitwise_not(self.hitOrMissCV(imagen)))
-
-        return thin
-"""
